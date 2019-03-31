02b28763d7463c2ece5d67847a1683b93cc43f19
Marcin Cytawa <marcin.cytawa@gmail.com>
2019-03-31 15:36:33 +0200

    "Some"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {Router, RouterModule, Routes} from '@angular/router';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
-  { path: 'servers', component: ServersComponent }
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,15 +23,17 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot( appRoutes )
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
